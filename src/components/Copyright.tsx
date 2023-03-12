14b3a1dcc630373470ad054f58cf46dde6fa8eda
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-03-12 13:51:50 +0000

    Added home page and linter options

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,21 @@
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import * as React from 'react';
+
+export function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        MAD Goat Project
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
